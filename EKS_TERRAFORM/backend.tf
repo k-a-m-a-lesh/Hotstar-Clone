92d0416a2d73ad2abb251e01ae1faf1f152fe970
k-a-m-a-lesh <166583222+k-a-m-a-lesh@users.noreply.github.com>
2024-07-14 22:16:10 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydealpha" # Replace with your actual S3 bucket name
+    bucket = "mybigbucket09" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "eu-north-1"
   }
